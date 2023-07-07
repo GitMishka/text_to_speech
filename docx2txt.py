ea8c8963547dca427577ea40779d6558937df276
Misha <mishagrigoryan@gmail.com>
2023-07-06 23:39:38 -0400

    broken

--- docx2txt.py
+++ docx2txt.py
@@ -1,24 +1,11 @@
 from flask import Flask, request, send_file
-import os
-from google.cloud import texttospeech
+from gtts import gTTS
 import docx2txt
+import os
 from PyPDF2 import PdfFileReader
 from docx import Document
 
 app = Flask(__name__)
-
-# List of available voices
-voices = [
-    texttospeech.VoiceSelectionParams(
-        language_code='en-US',
-        name='en-US-Wavenet-A',
-        ssml_gender=texttospeech.SsmlVoiceGender.FEMALE),
-    texttospeech.VoiceSelectionParams(
-        language_code='en-US',
-        name='en-US-Wavenet-D',
-        ssml_gender=texttospeech.SsmlVoiceGender.MALE),
-    # Add more voices here...
-]
 
 @app.route('/')
 def form():
@@ -28,10 +15,6 @@ def form():
         <h1>Text to Speech</h1>
         <form method="POST" action="convert" enctype="multipart/form-data">
             <input type="file" name="doc_file" accept=".docx,.doc,.pdf,.txt">
-            <select name="voice">
-                <option value="0">Female (en-US-Wavenet-A)</option>
-                <option value="1">Male (en-US-Wavenet-D)</option>
-            </select>
             <input type="submit">
         </form>
         </body>
@@ -54,20 +37,8 @@ def convert_text_to_speech():
     else:
         return "Invalid file type"
 
-    voice_index = request.form.get('voice')
-    voice = voices[int(voice_index)]
-
-    client = texttospeech.TextToSpeechClient()
-    synthesis_input = texttospeech.SynthesisInput(text=text)
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.MP3)
-
-    response = client.synthesize_speech(
-        input=synthesis_input, voice=voice, audio_config=audio_config)
-
-    # Save the audio to a file
-    with open('output.mp3', 'wb') as out:
-        out.write(response.audio_content)
+    speech = gTTS(text=text, lang='en', slow=False)
+    speech.save("output.mp3")
 
     # Return the audio file to the user
     return send_file("output.mp3", as_attachment=True)
